73703549d3b17b50cca8243e7c82019a35360133
Conhop22 <connor.hopper@magazinek12.com>
2024-05-09 22:11:16 -0500

    lhjk
    lhjk

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => (
+	<div className="footer">
+		Spyfall by{" "}
+		<a
+			href="https://conhop22.github.io/kjhg/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Tanner Krewson
+		</a>
+		<br />
+		<a
+			href="https://conhop22.github.io/kjhg/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			View on GitHub
+		</a>
+	</div>
+);
+
+export default Footer;
